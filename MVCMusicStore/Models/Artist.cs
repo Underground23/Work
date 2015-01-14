be5b230e2efb623317ce739a1446c3b535d9fe99
UnderGround23 <dina23@hotmail.com>
2015-01-14 18:11:50 +1100

    2  classes

---
+++ MVCMusicStore/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+
+        public String Name { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+}
